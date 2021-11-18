2ca270d5dee2c3c5353494fd7b825a9a97deacf1
Terrence Ryan <tpryan@google.com>
2021-11-17 18:39:53 -0800

    Building out CRUD

--- code/app/main.go
+++ code/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -32,8 +33,8 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api/v1/image", listHandler).Methods(http.MethodGet, http.MethodOptions)
 	// router.HandleFunc("/api/v1/image", createHandler).Methods(http.MethodPost)
-	// router.HandleFunc("/api/v1/image/{id}", readHandler).Methods(http.MethodGet)
-	// router.HandleFunc("/api/v1/image/{id}", deleteHandler).Methods(http.MethodDelete)
+	router.HandleFunc("/api/v1/image/{id}", readHandler).Methods(http.MethodGet)
+	router.HandleFunc("/api/v1/image/{id}", deleteHandler).Methods(http.MethodDelete)
 	// router.HandleFunc("/api/v1/image/{id}", updateHandler).Methods(http.MethodPost, http.MethodPut)
 
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
@@ -59,6 +60,41 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 
 	writeJSON(w, is, http.StatusOK)
 	return
+}
+
+func readHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	fs, err := cs.Read(id)
+	if err != nil {
+		writeErrorMsg(w, fmt.Errorf("failed to read files %s: %v", id, err))
+
+		return
+	}
+
+	is, err := NewImages(fs)
+	if err != nil {
+		writeErrorMsg(w, fmt.Errorf("failed to convert files to images images: %v", err))
+		return
+	}
+	if len(is) < 1 {
+		writeResponse(w, http.StatusNoContent, "")
+		return
+	}
+
+	writeJSON(w, is[0], http.StatusOK)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	if err := cs.Delete(id); err != nil {
+		writeErrorMsg(w, err)
+		return
+	}
+	msg := Message{"image deleted", fmt.Sprintf("image id: %s", id)}
+
+	writeJSON(w, msg, http.StatusNoContent)
 }
 
 // JSONProducer is an interface that spits out a JSON string version of itself
@@ -98,4 +134,30 @@ func writeResponse(w http.ResponseWriter, status int, msg string) {
 
 func weblog(msg string) {
 	log.Printf("Webserver : %s", msg)
+}
+
+// Message is a structure for communicating additional data to API consumer.
+type Message struct {
+	Text    string `json:"text"`
+	Details string `json:"details"`
+}
+
+// JSON marshalls the content of a todo to json.
+func (m Message) JSON() (string, error) {
+	bytes, err := json.Marshal(m)
+	if err != nil {
+		return "", fmt.Errorf("could not marshal json for response: %s", err)
+	}
+
+	return string(bytes), nil
+}
+
+// JSONBytes marshalls the content of a todo to json as a byte array.
+func (m Message) JSONBytes() ([]byte, error) {
+	bytes, err := json.Marshal(m)
+	if err != nil {
+		return []byte{}, fmt.Errorf("could not marshal json for response: %s", err)
+	}
+
+	return bytes, nil
 }
--- code/app/storage.go
+++ code/app/storage.go
@@ -64,6 +64,57 @@ func (cs CloudStorage) List() (CSFiles, error) {
 	return i, nil
 }
 
+func (cs CloudStorage) Read(id string) (CSFiles, error) {
+	i := CSFiles{}
+	bucket := cs.Client.Bucket(cs.Bucket)
+
+	query := &storage.Query{Prefix: fmt.Sprintf("processed/%s", id)}
+	it := bucket.Objects(cs.ctx, query)
+	for {
+		obj, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return i, fmt.Errorf("error iterating over bucket query: %s", err)
+		}
+
+		u, err := url.Parse(obj.MediaLink)
+		if err != nil {
+			return i, fmt.Errorf("cannot create url from %s: %s", obj.MediaLink, err)
+		}
+		img := CSFile{obj.Name, cs.Bucket, u}
+		i = append(i, img)
+
+	}
+
+	return i, nil
+}
+
+func (cs CloudStorage) Delete(id string) error {
+	bucket := cs.Client.Bucket(cs.Bucket)
+	query := &storage.Query{Prefix: fmt.Sprintf("processed/%s/", id)}
+	it := bucket.Objects(cs.ctx, query)
+	for {
+		i, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("error iterating over bucket query: %s", err)
+		}
+
+		obj := cs.Client.Bucket(cs.Bucket).Object(i.Name)
+
+		if err := obj.Delete(cs.ctx); err != nil {
+			return fmt.Errorf("error deleting  %s: %s", i.Name, err)
+		}
+
+	}
+
+	return nil
+}
+
 type CSFile struct {
 	Name   string
 	Bucket string
@@ -78,23 +129,59 @@ type Image struct {
 	Thumbnail string `json:"thumbnail"`
 }
 
+// Load converts a Cloud Storage Object to the format we need for this app.
+func (i *Image) Load(f CSFile) error {
+	if strings.Index(f.Name, "original.") > -1 {
+		dir := filepath.Dir(f.Name)
+		base := filepath.Base(f.Name)
+		name := strings.Replace(dir, "processed/", "", 1)
+		o := fmt.Sprintf("https://storage.googleapis.com/%s/%s/%s", f.Bucket, dir, base)
+		t := fmt.Sprintf("https://storage.googleapis.com/%s/%s/%s", f.Bucket, dir, strings.Replace(base, "original.", "thumbnail.", 1))
+		img := Image{name, o, t}
+		*i = img
+	}
+
+	return nil
+}
+
+// JSON marshalls the content of Image to json.
+func (i Image) JSON() (string, error) {
+	bytes, err := json.Marshal(i)
+	if err != nil {
+		return "", fmt.Errorf("could not marshal json for response: %s", err)
+	}
+
+	return string(bytes), nil
+}
+
+// JSONBytes marshalls the content of Image to json.
+func (i Image) JSONBytes() ([]byte, error) {
+	bytes, err := json.Marshal(i)
+	if err != nil {
+		return []byte{}, fmt.Errorf("could not marshal json for response: %s", err)
+	}
+
+	return bytes, nil
+}
+
 type Images []Image
 
+// NewImages returns a list of images in the format we need for this app.
 func NewImages(fs CSFiles) (Images, error) {
 	is := Images{}
 	err := is.Load(fs)
 	return is, err
 }
 
+// Load converts a slice of Cloud Storage Objects to the format we need for this app.
 func (is *Images) Load(fs CSFiles) error {
 	for _, v := range fs {
 		if strings.Index(v.Name, "original.") > -1 {
-			dir := filepath.Dir(v.Name)
-			base := filepath.Base(v.Name)
-			name := strings.Replace(dir, "processed/", "", 1)
-			o := fmt.Sprintf("https://storage.googleapis.com/%s/%s/%s", v.Bucket, dir, base)
-			t := fmt.Sprintf("https://storage.googleapis.com/%s/%s/%s", v.Bucket, dir, strings.Replace(base, "original.", "thumbnail.", 1))
-			i := Image{name, o, t}
+			i := Image{}
+			if err := i.Load(v); err != nil {
+				return err
+			}
+
 			*is = append(*is, i)
 		}
 	}
